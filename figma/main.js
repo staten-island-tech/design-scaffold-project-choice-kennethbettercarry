9e61ad1f03d371ce818c693feceb02d600b3417b
Wilson-Wang1 <90658753+Wilson-Wang1@users.noreply.github.com>
2022-03-28 13:11:05 -0400

    fdg

--- figma/main.js
+++ figma/main.js
@@ -2,4 +2,3 @@ import "./style.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-gsap.to(".box", { rotation: 27, x: 100, duration: 1 });
