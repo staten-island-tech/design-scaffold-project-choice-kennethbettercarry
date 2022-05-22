d91a379f9224f9b1a7414a5a626e054b9770a405
Wilson-Wang1 <90658753+Wilson-Wang1@users.noreply.github.com>
2022-05-22 18:17:47 -0400

    lowkey confused

--- figma/main.js
+++ figma/main.js
@@ -2,3 +2,11 @@ import "./style.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+const navWorking = () => {
+  const Three = document.querySelector(".ESC");
+  const Nav = document.querySelector(".navlisted");
+};
+Three.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+});
+navWorking();
