59753d07bf23761343c39a032c26d80eb9ccb2f6
Wilson-Wang1 <90658753+Wilson-Wang1@users.noreply.github.com>
2022-05-22 21:03:45 -0400

    nav bar tutorial done

--- figma/main.js
+++ figma/main.js
@@ -2,11 +2,26 @@ import "./style.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-const navWorking = () => {
-  const Three = document.querySelector(".ESC");
-  const Nav = document.querySelector(".navlisted");
+//NavBarThings
+const navSlide = () => {
+  const Lined = document.querySelector(".Lined");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  Lined.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+    //The line animated
+    Lined.classList.toggle("toggle");
+  });
 };
-Three.addEventListener("click", () => {
-  nav.classList.toggle("nav-active");
-});
-navWorking();
+
+navSlide();
