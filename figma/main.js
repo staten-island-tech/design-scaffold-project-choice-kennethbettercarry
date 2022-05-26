890fbcfdc682b8e9bc9c86aa4b9575146bd1f2e4
Wilson-Wang1 <90658753+Wilson-Wang1@users.noreply.github.com>
2022-05-25 22:46:17 -0400

    bruh toggle
    doesnt work?

--- figma/main.js
+++ figma/main.js
@@ -48,3 +48,23 @@ const tl = gsap.timeline({ scrollTrigger: ".emptysections", delay: 1.0 });
 
 tl.from(".header", { opacity: 0, duration: 0.2 });
 tl.from(".description", { opacity: 0, duration: 0.5 });
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+toggleSwitch.addEventListener("change", switchTheme, false);
